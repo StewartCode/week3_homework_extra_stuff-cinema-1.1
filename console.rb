649294b95cc0f9baa317e0a10de719e5d383b24f
StevieCode <sdcowan@hotmail.com>
2018-10-22 09:25:18 +0100

    1.5

--- console.rb
+++ console.rb
@@ -251,63 +251,8 @@ puts  "Thanks for playing"
 # puts film1a.customers
 #________________________________________
 
-  # customer1 = Customer.new({
-  #   'name' => 'bob smith',
-  #   'funds' => 20
-  # })
-  #
-  # customer1.save()
-  #
-  #
-  # customer2 = Customer.new({
-  #   'name' => 'jim hunter',
-  #   'funds' => 40
-  # })
-  #
-  # customer2.save()
-  #
-  # customer3 = Customer.new({
-  #   'name' => 'gareth miles',
-  #   'funds' => 120
-  # })
-  #
-  # customer3.save()
-  #
-  # customer4 = Customer.new({
-  #   'name' => 'jimmy cranky',
-  #   'funds' => 39
-  # })
-  #
-  # customer4.save()
-  #
-  # ticket1 = Ticket.new({'film_id' => film1.id, 'customer_id' => customer1.id})
-  # ticket2 = Ticket.new({'film_id' => film2.id, 'customer_id' => customer2.id})
-  # ticket3 = Ticket.new({'film_id' => film3.id, 'customer_id' => customer3.id})
-  # ticket4 = Ticket.new({'film_id' => film4.id, 'customer_id' => customer4.id})
-  # ticket5 = Ticket.new({'film_id' => film2.id, 'customer_id' => customer1.id})
-  # ticket6 = Ticket.new({'film_id' => film3.id, 'customer_id' => customer2.id})
 
 
- # ticket1.save()
- # ticket2.save()
- # ticket3.save()
- # ticket4.save()
- # ticket5.save()
- # ticket6.save()
- #
- # Ticket.all()
- # Film.all()
- # Customer.all()
- #
- # customer1.films
- #
- # film2.customers
-
- # customer1.name = "test name"
- # customer1.update
-
- # film1.title = "test name"
- # film1.update
 
 binding.pry
 nil
